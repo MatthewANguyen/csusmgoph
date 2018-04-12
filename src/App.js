46c707fec97329b868d68249d429d67b6c98ab04
matthewnguyen <matthewnguyen19@gmail.com>
2018-04-11 22:26:04 -0700

    directories

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Nav from './components/Nav/Nav.js';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Nav/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+class Nav extends React.Component {
+    render() {
+        return(
+            <nav>
+                <div className="container">
+                    <div className='row nav-row'>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1'>
+
+                        </div>
+                        <div className='col-md-1 link'>
+                            About
+                        </div>
+                        <div className='col-md-1 link'>
+                            Portfolio
+                        </div>
+                        <div className='col-md-1 link'>
+                            Contact
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
