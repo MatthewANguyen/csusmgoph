3003773555088adce2fe0c52b81c4536cf0b38a1
JuliusDorfman <juliusdorfman@gmail.com>
2018-04-28 15:58:51 -0700

    UPDATE/PUT route working

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const PORT = 3000 || process.env.PORT();
 
+// Initialize bodyParser
+app.use(bodyParser.json());
 
 // Initialize Mongoose
 let Event = require('./src/models/event');
@@ -13,34 +15,69 @@ const db = mongoose.connection
 
 // Landing page route
 app.get('/', function(req, res) {
-	res.send('WELCOME TO LANDING PAGE. USE /api/events FOR EVENTS');
+    res.send('WELCOME TO LANDING PAGE. USE /api/events FOR EVENTS');
 })
 
 // api for events
+
+// Find all Events
+
 app.get('/api/events', function(req, res) {
-	Event.getEvents(function(err, events) {
-		if(err){ 
-			throw err;
-		}
-		console.log("/api/events")		
-		res.json(events);
-	});
+    Event.getEvents(function(err, events) {
+        if (err) {
+            console.log("err", err)
+            throw err;
+        }
+        console.log("/api/events")
+        res.json(events);
+    });
 });
 
+//  Get One Event
+
+app.get('/api/events/:_id', function(req, res) {
+    Event.getEvent(req.params._id, function(err, event) {
+        if (err) {
+            console.log("err", err)
+            throw err;
+        }
+        console.log("/api/events/:_id")
+        res.json(event);
+    });
+});
+
+// Post New Event
+
 app.post('/api/events', function(req, res) {
-	var genre = req.body;
-	Event.addEvent(event, function(err, event) {
-		if(err){ 
+    var event = req.body;
+    console.log('event initialize post req', event);
+    Event.addEvent(event, function(err, event) {
+        if (err) {
+            console.log("err", err)
+        	console.log('event ERROR', event);
+            throw err;
+        }
+        console.log('event return event json', event);
+        res.json(event);
+    });
+});
+
+// Update Event
+
+app.put('/api/events/:id', function(req, res) {
+	var id = req.params.id;
+	var event = req.body;
+	console.log("req, params,id", req.params);
+	Event.updateEvent(id, event, {}, function(err, event) {
+		if(err) {
 			throw err;
 		}
-		console.log("/api/event")		
 		res.json(event);
 	});
 });
 
 
-
 // Listening for changes on this PORT
-app.listen(PORT, function(){
-	console.log("Listening on PORT: " + PORT);
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
 });
--- src/models/event.js
+++ src/models/event.js
@@ -3,27 +3,42 @@ const mongoose = require('mongoose');
 
 // Event Schema
 var eventSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-		require: true
-	},
-	create_date: {
-		type: Date,
-		default: Date.now
-	}
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 var Event = module.exports = mongoose.model('Event', eventSchema);
 
 // Get Events
 module.exports.getEvents = function(callback, limit) {
-	Event.find(callback).limit(limit);
+    Event.find(callback).limit(limit);
 };
 
-module.exports.addEvent = function(callback, limit) {
-	Event.find(callback);
+// Get Event
+module.exports.getEvent = function(id, callback) {
+    Event.findById(id, callback);
+};
+
+// Add Event
+module.exports.addEvent = function(event, callback) {
+    Event.create(event, callback);
+};
+
+// Update Event
+module.exports.updateEvent = function(id, event, options, callback) {
+    var query = { _id: id };
+    var update = {
+        name: event.name
+    }
+    Event.findOneAndUpdate(query, update, options, callback);
 };
